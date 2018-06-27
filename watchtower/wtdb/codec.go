1fdef1f6581e1a0dacb327e84c2e429359d3ae15
Conner Fromknecht <conner@mit.edu>
2018-06-27 16:48:11 -0700

    watchtower/wtdb/codec: extend channeldb codec

---
+++ watchtower/wtdb/codec.go
@@ -0,0 +1,57 @@
+package wtdb
+
+import (
+	"github.com/lightningnetwork/lnd/channeldb"
+)
+
+func WriteElement(w io.Writer, element interface{}) error {
+	werr := channeldb.WriteElement(w, element)
+	switch werr.(type) {
+	case *channeldb.UnknownElementType:
+	default:
+		return werr
+	}
+
+	switch e := element.(type) {
+	// TODO(conner): add types
+	}
+
+	return nil
+}
+
+func WriteElements(w io.Writer, elements ...interface{}) error {
+	for _, element := range elements {
+		err := WriteElement(w, element)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func ReadElement(r io.Reader, element interface{}) error {
+	rerr := channeldb.ReadElement(r, element)
+	switch rerr.(type) {
+	case *channeldb.UnknownElementType:
+	default:
+		return rerr
+	}
+
+	switch e := element.(type) {
+	// TODO(conner): add types
+	}
+
+	return nil
+}
+
+func ReadElements(r io.Reader, element ...interface{}) error {
+	for _, element := range elements {
+		err := ReadElement(r, element)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
