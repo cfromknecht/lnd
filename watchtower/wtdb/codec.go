581ce44e8e7b4369dba2e012639adb195608d65b
Conner Fromknecht <conner@mit.edu>
2018-06-28 14:02:09 -0700

    watchtower/wtdb/codec: add wire.BitcoinNet to codec

--- watchtower/wtdb/codec.go
+++ watchtower/wtdb/codec.go
@@ -1,7 +1,10 @@
 package wtdb
 
 import (
+	"io"
+
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/roasbeef/btcd/wire"
 )
 
 func WriteElement(w io.Writer, element interface{}) error {
@@ -14,10 +17,16 @@ func WriteElement(w io.Writer, element interface{}) error {
 
 	switch e := element.(type) {
 	// TODO(conner): add types
+	case wire.BitcoinNet:
+		if err := channeldb.WriteElement(w, uint32(e)); err != nil {
+			return err
+		}
+
 	case BreachHint:
 		if _, err := w.Write(e[:]); err != nil {
 			return err
 		}
+
 	case BreachKey:
 		if _, err := w.Write(e[:]); err != nil {
 			return err
@@ -48,10 +57,18 @@ func ReadElement(r io.Reader, element interface{}) error {
 
 	switch e := element.(type) {
 	// TODO(conner): add types
+	case *wire.BitcoinNet:
+		var network uint32
+		if err := channeldb.ReadElement(r, &network); err != nil {
+			return err
+		}
+		*e = wire.BitcoinNet(network)
+
 	case *BreachHint:
 		if _, err := io.ReadFull(r, e[:]); err != nil {
 			return err
 		}
+
 	case *BreachKey:
 		if _, err := io.ReadFull(r, e[:]); err != nil {
 			return err
