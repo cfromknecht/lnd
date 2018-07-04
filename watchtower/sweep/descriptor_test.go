6b2336c673a281068f9e94df7514aa604a0d275a
Conner Fromknecht <conner@mit.edu>
2018-07-03 19:32:46 -0700

    watchtower/sweep/descriptor_test: expand unit tests

--- watchtower/sweep/descriptor_test.go
+++ watchtower/sweep/descriptor_test.go
@@ -4,7 +4,6 @@ import (
 	"crypto/rand"
 	"encoding/binary"
 	"io"
-	"log"
 	"reflect"
 	"testing"
 
@@ -24,15 +23,20 @@ func newPubKey(i uint64) *sweep.PubKey {
 }
 
 var descriptorTests = []struct {
-	name          string
-	encVersion    uint16
-	decVersion    uint16
-	revPubKey     sweep.PubKey
-	delayPubKey   sweep.PubKey
-	csvDelay      uint32
-	p2wkhPubKey   *sweep.PubKey
-	encVersionErr error
-	decVersionErr error
+	name             string
+	encVersion       uint16
+	decVersion       uint16
+	revPubKey        sweep.PubKey
+	delayPubKey      sweep.PubKey
+	csvDelay         uint32
+	hasP2wkhPubKey   bool
+	p2wkhPubKey      *sweep.PubKey
+	numOfferedHltcs  uint16
+	numReceivedHtlcs uint16
+	remoteHtlcPubKey *sweep.PubKey
+	localHtlcPubKey  *sweep.PubKey
+	encErr           error
+	decErr           error
 }{
 	{
 		name:        "to-local output only",
@@ -43,44 +47,146 @@ var descriptorTests = []struct {
 		csvDelay:    144,
 	},
 	{
-		name:          "unknown encrypt version",
-		encVersion:    1,
-		decVersion:    0,
-		revPubKey:     makePubKey(0),
-		delayPubKey:   makePubKey(1),
-		csvDelay:      144,
-		encVersionErr: sweep.ErrUnknownBlobVersion,
-	},
-	{
-		name:          "unknown decrypt version",
-		encVersion:    0,
-		decVersion:    1,
-		revPubKey:     makePubKey(0),
-		delayPubKey:   makePubKey(1),
-		csvDelay:      144,
-		decVersionErr: sweep.ErrUnknownBlobVersion,
-	},
-	{
-		name:        "to-local and p2wkh outputs",
-		encVersion:  0,
+		name:        "unknown encrypt version",
+		encVersion:  1,
 		decVersion:  0,
 		revPubKey:   makePubKey(0),
 		delayPubKey: makePubKey(1),
 		csvDelay:    144,
-		p2wkhPubKey: newPubKey(2),
+		encErr:      sweep.ErrUnknownBlobVersion,
+	},
+	{
+		name:        "unknown decrypt version",
+		encVersion:  0,
+		decVersion:  1,
+		revPubKey:   makePubKey(0),
+		delayPubKey: makePubKey(1),
+		csvDelay:    144,
+		decErr:      sweep.ErrUnknownBlobVersion,
+	},
+	{
+		name:           "to-local and p2wkh outputs",
+		encVersion:     0,
+		decVersion:     0,
+		revPubKey:      makePubKey(0),
+		delayPubKey:    makePubKey(1),
+		csvDelay:       144,
+		hasP2wkhPubKey: true,
+		encErr:         sweep.ErrMissingP2WKHPubKey,
+	},
+	{
+		name:           "to-local and p2wkh outputs",
+		encVersion:     0,
+		decVersion:     0,
+		revPubKey:      makePubKey(0),
+		delayPubKey:    makePubKey(1),
+		csvDelay:       144,
+		hasP2wkhPubKey: true,
+		p2wkhPubKey:    newPubKey(2),
+	},
+	{
+		name:            "to-local, p2wkh, and htlc outputs",
+		encVersion:      0,
+		decVersion:      0,
+		revPubKey:       makePubKey(0),
+		delayPubKey:     makePubKey(1),
+		csvDelay:        144,
+		hasP2wkhPubKey:  true,
+		p2wkhPubKey:     newPubKey(2),
+		numOfferedHltcs: 1,
+		encErr:          sweep.ErrMissingHtlcPubKey,
+	},
+	{
+		name:             "to-local, p2wkh, and htlc outputs",
+		encVersion:       0,
+		decVersion:       0,
+		revPubKey:        makePubKey(0),
+		delayPubKey:      makePubKey(1),
+		csvDelay:         144,
+		hasP2wkhPubKey:   true,
+		p2wkhPubKey:      newPubKey(2),
+		numReceivedHtlcs: 1,
+		encErr:           sweep.ErrMissingHtlcPubKey,
+	},
+	{
+		name:             "to-local, p2wkh, and htlc outputs",
+		encVersion:       0,
+		decVersion:       0,
+		revPubKey:        makePubKey(0),
+		delayPubKey:      makePubKey(1),
+		csvDelay:         144,
+		hasP2wkhPubKey:   true,
+		p2wkhPubKey:      newPubKey(2),
+		numOfferedHltcs:  1,
+		remoteHtlcPubKey: newPubKey(3),
+		encErr:           sweep.ErrMissingHtlcPubKey,
+	},
+	{
+		name:             "to-local, p2wkh, and htlc outputs",
+		encVersion:       0,
+		decVersion:       0,
+		revPubKey:        makePubKey(0),
+		delayPubKey:      makePubKey(1),
+		csvDelay:         144,
+		hasP2wkhPubKey:   true,
+		p2wkhPubKey:      newPubKey(2),
+		numReceivedHtlcs: 1,
+		localHtlcPubKey:  newPubKey(3),
+		encErr:           sweep.ErrMissingHtlcPubKey,
+	},
+	{
+		name:             "to-local, p2wkh, and htlc outputs",
+		encVersion:       0,
+		decVersion:       0,
+		revPubKey:        makePubKey(0),
+		delayPubKey:      makePubKey(1),
+		csvDelay:         144,
+		hasP2wkhPubKey:   true,
+		p2wkhPubKey:      newPubKey(2),
+		numReceivedHtlcs: 1,
+		remoteHtlcPubKey: newPubKey(3),
+		localHtlcPubKey:  newPubKey(4),
+	},
+	{
+		name:             "to-local, p2wkh, and htlc outputs",
+		encVersion:       0,
+		decVersion:       0,
+		revPubKey:        makePubKey(0),
+		delayPubKey:      makePubKey(1),
+		csvDelay:         144,
+		hasP2wkhPubKey:   true,
+		p2wkhPubKey:      newPubKey(2),
+		numOfferedHltcs:  2,
+		numReceivedHtlcs: 2,
+		remoteHtlcPubKey: newPubKey(3),
+		localHtlcPubKey:  newPubKey(4),
+	},
+	{
+		name:             "to-local, and htlc outputs",
+		encVersion:       0,
+		decVersion:       0,
+		revPubKey:        makePubKey(0),
+		delayPubKey:      makePubKey(1),
+		csvDelay:         144,
+		numOfferedHltcs:  2,
+		numReceivedHtlcs: 2,
+		remoteHtlcPubKey: newPubKey(3),
+		localHtlcPubKey:  newPubKey(4),
 	},
 }
 
 func TestSweepDescriptorEncryptDecrypt(t *testing.T) {
 	for i, test := range descriptorTests {
 		desc := &sweep.Descriptor{
-			HasP2WKHOutput: test.p2wkhPubKey != nil,
-			Params: sweep.StaticScriptParams{
-				RevocationPubKey: test.revPubKey,
-				LocalDelayPubKey: test.delayPubKey,
-				CSVDelay:         test.csvDelay,
-				P2WKHPubKey:      test.p2wkhPubKey,
-			},
+			RevocationPubKey: test.revPubKey,
+			LocalDelayPubKey: test.delayPubKey,
+			CSVDelay:         test.csvDelay,
+			HasP2WKHOutput:   test.hasP2wkhPubKey,
+			P2WKHPubKey:      test.p2wkhPubKey,
+			NumOfferedHtlcs:  test.numOfferedHltcs,
+			NumReceivedHtlcs: test.numReceivedHtlcs,
+			RemoteHtlcPubKey: test.remoteHtlcPubKey,
+			LocalHtlcPubKey:  test.localHtlcPubKey,
 		}
 
 		// Generate a random encryption key for the blob. The key is
@@ -96,10 +202,10 @@ func TestSweepDescriptorEncryptDecrypt(t *testing.T) {
 		// Encrypt the sweep descriptor using the generated key and
 		// target version for this test.
 		blob, err := desc.Encrypt(key, test.encVersion)
-		if err != test.encVersionErr {
+		if err != test.encErr {
 			t.Fatalf("test #%d %s -- unable to encrypt blob: %v",
 				i, test.name, err)
-		} else if test.encVersionErr != nil {
+		} else if test.encErr != nil {
 			// If the test expected an encryption failure, we can
 			// continue to the next test.
 			continue
@@ -109,10 +215,10 @@ func TestSweepDescriptorEncryptDecrypt(t *testing.T) {
 		// descriptor from the decrypted contents. We use the target
 		// decryption version specified by this test case.
 		desc2, err := sweep.DescriptorFromBlob(blob, key, test.decVersion)
-		if err != test.decVersionErr {
+		if err != test.decErr {
 			t.Fatalf("test #%d %s -- unable to decrypt blob: %v",
 				i, test.name, err)
-		} else if test.decVersionErr != nil {
+		} else if test.decErr != nil {
 			// If the test expected an decryption failure, we can
 			// continue to the next test.
 			continue
