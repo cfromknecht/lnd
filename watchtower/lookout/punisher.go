f3e69c2b93545b679dba9009ce2433c37e91c3f8
Conner Fromknecht <conner@lightning.engineering>
2018-12-19 21:50:42 -0800

    watchtower/lookout: correct txid logging

--- watchtower/lookout/punisher.go
+++ watchtower/lookout/punisher.go
@@ -35,18 +35,18 @@ func (p *BreachPunisher) Punish(desc *JusticeDescriptor, quit <-chan struct{}) e
 	justiceTxn, err := desc.CreateJusticeTxn()
 	if err != nil {
 		log.Errorf("Unable to create justice txn for "+
-			"client=%s with breach-txid=%x: %v",
+			"client=%s with breach-txid=%s: %v",
 			desc.SessionInfo.ID, desc.BreachedCommitTx.TxHash(), err)
 		return err
 	}
 
-	log.Infof("Publishing justice transaction txid=%x for client=%s",
+	log.Infof("Publishing justice transaction txid=%s for client=%s",
 		justiceTxn.TxHash(), desc.SessionInfo.ID)
 
 	err = p.cfg.PublishTx(justiceTxn)
 	if err != nil && err != lnwallet.ErrDoubleSpend {
 		log.Errorf("Unable to publish justice txn for client=%s"+
-			"with breach-txid=%x: %v",
+			"with breach-txid=%s: %v",
 			desc.SessionInfo.ID, desc.BreachedCommitTx.TxHash(), err)
 		return err
 	}
